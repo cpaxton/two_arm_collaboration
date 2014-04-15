9d5130ccd7d4a47efa6c5b2f9febd834cbd5b975
Christopher Paxton <cpaxton3@jhu.edu>
2014-04-15 17:12:23 -0400

    inital commit; simple plugin

---
+++ src/plugin.cpp
@@ -0,0 +1,43 @@
+#include <ros/ros.h>
+#include <boost/bind.hpp>
+#include <gazebo/gazebo.hh>
+#include <gazebo/physics/physics.hh>
+#include <gazebo/common/common.hh>
+#include <stdio.h>
+
+namespace gazebo
+{   
+  class ModelJointStatePublisher : public ModelPlugin
+  {
+
+    ros::NodeHandle nh_; // ros node handle
+    ros::Publisher pub_; // ros joint state publisher
+
+    public: void Load(physics::ModelPtr _parent, sdf::ElementPtr /*_sdf*/) 
+    {
+      // Store the pointer to the model
+      this->model = _parent;
+
+      // Listen to the update event. This event is broadcast every
+      // simulation iteration.
+      this->updateConnection = event::Events::ConnectWorldUpdateBegin(
+          boost::bind(&ModelJointStatePublisher::OnUpdate, this, _1));
+    }
+
+    // Called by the world update start event
+    public: void OnUpdate(const common::UpdateInfo & /*_info*/)
+    {
+
+
+    }
+
+    // Pointer to the model
+    private: physics::ModelPtr model;
+
+    // Pointer to the update event connection
+    private: event::ConnectionPtr updateConnection;
+  };
+
+  // Register this plugin with the simulator
+  GZ_REGISTER_MODEL_PLUGIN(ModelJointStatePublisher)
+}
