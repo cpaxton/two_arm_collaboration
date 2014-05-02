4b8d1944fcde90bbe4ad53e24f51b74aba3cdd7d
Christopher Paxton <cpaxton3@jhu.edu>
2014-05-02 11:45:05 -0400

    bag files read

--- src/read.hpp
+++ src/read.hpp
@@ -9,42 +9,93 @@
 // ROS includes
 #include <ros/ros.h>
 #include <rosbag/bag.h>
+#include <rosbag/view.h>
+#include <message_filters/subscriber.h>
+#include <message_filters/time_synchronizer.h>
 
 // LCSR includes
 #include <oro_barrett_msgs/BHandCmd.h>
 
 namespace lcsr_replay {
 
+  /* typedef to avoid worrying about templates just yet */
+  typedef oro_barrett_msgs::BHandCmd discrete_msg_t;
+
   /**
    * DemoReader
    * Read a demonstration from ROSBAG file.
    * Extract features.
    * */
-  template <discrete_msg_t>
   class DemoReader : rosbag::Bag {
+  private:
+    int verbosity;
+
+    const int DEFAULT_RATE = 60;
+
   protected:
     
+    std::vector<std::string> topics; // list of all topics
+
+    ros::NodeHandle nh; // default namespace for publishers
+    ros::NodeHandle nh_tilde; // private namespace
+
     // record certain things which we just send as-is at times, we do not interpret or warp them
-    std::map<std::string, discrete_msg_t> discrete_topics;
+    std::vector<ros::Publisher> discrete_topics;
 
   public:
 
-    DemoReader(const std::string &bagfile) : rosbag::Bag(bagfile) {
+    DemoReader(const std::string &bagfile) : rosbag::Bag(bagfile), nh(), nh_tilde("~") {
 
+      nh_tilde.param("verbosity", verbosity, int(1));
+
+      if(verbosity > 0) {
+        ROS_INFO("Initializing Demonstration Reader...");
+      }
     }
 
     /* addDiscreteTopic()
      * Add a discrete topic.
      * For these, we look at what messages get sent and when.
      * We repeat them when the right conditions are met.
+     * 
+     * FUTURE PLANS: template function that adds a ROS publisher?
      */
-    addDiscreteTopic(const std::string &topic);
+    //template<class T = discrete_msg_t>
+    int addDiscreteTopic(const std::string &topic, int rate = DEFAULT_RATE) {
+      topics.push_back(topic);
+
+      ros::Publisher pub = nh.advertise<discrete_msg_t>(topic, rate);
+
+      if(verbosity > 0) {
+        ROS_INFO("Adding discrete topic %s, publishing at rate %d", topic.c_str(), rate);
+      }
+
+      return 1;
+    }
+
+
 
     /* addTopic()
      * Add a continuous/inverse kinematics command topic.
      * These are interpreted according to feature counts.
      */
-    addTopic(const std::string &topic);
+    int addTopic(const std::string &topic, int rate = DEFAULT_RATE) {
+      topics.push_back(topic);
+
+      if(verbosity > 0) {
+        ROS_INFO("Adding topic %s, publishing at rate %d", topic.c_str(), rate);
+      }
+
+      return 1;
+    }
+
+    int examine() {
+      rosbag::View view((rosbag::Bag *this, rosbag::TopicQuery(topics)));
+
+      foreach(rosbag::MessageInstance const m, view) {
+        ROS_INFO("topic=\"%s\", time=%f", m.getTopic().c_str(), m.getTime().toSec());
+      }
+    }
     
   }
 
---
+++ src/test.cpp
@@ -0,0 +1,31 @@
+#include <ros/ros.h>
+
+#include "read.hpp"
+
+int main(int argc, char **argv) {
+
+  ros::init(argc, argv, "lcsr_replay_test");
+  ros::NodeHandle nh("~");
+
+  // define topics
+  std::string bagfile;
+  std::string arm1_topic;
+  std::string arm2_topic;
+  std::sting bhand1_topic;
+  std::string bhand2_topic;
+
+  ROS_INFO("Reading topics...");
+  nh.param("bag",bagfile,std::string("demo.bag"));
+  nh.param("bhand1_topic",bhand1_topic,std::string("gazebo/barrett_manager/hand/cmd"));
+  nh.param("bhand2_topic",bhand1_topic,std::string("gazebo/w2barrett_manager/hand/cmd"));
+  nh.param("arm1_topic", arm1_topic,std::string("wam/cmd"));
+  nh.param("arm2_topic",arm2_topic,std::string("wam2/cmd"));
+
+
+  DemoReader dr(bagfile);
+  dr.addDiscreteTopic(bhand1_topic);
+  dr.addDiscreteTopic(bhand2_topic);
+  dr.addTopic(arm1_topic);
+  dr.addTopic(arm2_topic);
+
+}
