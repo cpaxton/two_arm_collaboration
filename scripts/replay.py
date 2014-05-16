a9d9da9ea521e753346b94cd4b71e4de07074fb3
Christopher Paxton <cpaxton3@jhu.edu>
2014-05-16 11:32:09 -0400

    added more to python file

--- scripts/replay.py
+++ scripts/replay.py
@@ -4,6 +4,8 @@ import rospy
 import rosbag # reading bag files
 
 from lcsr_replay.msg import *
+from dmp.msg import *
+from dmp.srv import *
 
 '''
 REPLAY.PY
@@ -17,13 +19,34 @@ class ReplayIO:
     bagfile = 'demo.bag'
     io_topics = ['wam/cmd', 'wam2/cmd', '/FEATURES', '/SEGMENT']
 
+    segment = []
+    data = {}
+    parsed = False
+
     '''
     parse()
     Takes a filename, opens the bag up.
     Should also parse out necessary information by segment.
     '''
-    def parse(filename) :
+    def parse(filename = bagfile) :
         bagfile = filename
+
+        segment = []
+        data = {}
+        for topic in io_topics:
+            data[topic] = []
+
+        for topic, msg, t in bag.read_messages(io_topics) :
+
+            if topic == "/SEGMENT" :
+                data[topic] += [msg.num]
+            else if topic == "/FEATURES" :
+                print msg
+
+            # TODO: convert format to 
+            data{topic} += [msg]
+
+        parsed = True
 
 
     '''
@@ -42,7 +65,30 @@ class ReplayIO:
         for topic, msg, t in bag.read_messages(io_topics) :
             print "%f: %s"%(t, topic)
 
+    def getTrajectory(topic) :
+
+        if parsed == False :
+            parse()
+
+        bag = rosbag.Bag(bagfile)
+           
+        return data[topic]
+
+'''
+default_io_startup()
+Default startup for the Replay package.
+Reads parameter from command line and sets up an IO object.
+'''
+def default_io_startup():
+
+    bagfile = rospy.getParam('bag', 'demo.bag')
+
+    io = ReplayIO()
+    io.setFilename(bagfile)
 
 if __name__ == '__main__':
     rospy.init_node('replay_read_node')
+
+    io = default_io_startup()
+
 
