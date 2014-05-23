4b68543a460a1fddad61b5583a34ff5eb411087f
Christopher Paxton <cpaxton3@jhu.edu>
2014-05-23 13:35:14 -0400

    SMACH task scrip

---
+++ execution/task.py
@@ -0,0 +1,73 @@
+#!/usr/bin/env python
+
+import roslib; roslib.load_manifest('peg_assist_demo')
+import rospy
+import smach
+import smach_ros
+
+human_arm = 'wam'
+auto_arm = 'wam2'
+
+class WaitForRing (smach.State):
+    def __init__(self):
+        smach.State.__init__(self, outcomes=['success'])
+        self.result = 0
+
+    def execute(self, userdata):
+        rospy.loginfo('Waiting for Ring...')
+        return 'success'
+
+
+class GrabRing(smach.State):
+    def __init__(self):
+        smach.State.__init__(self, outcomes=['success'])
+
+    def execute(self, userdata):
+        rospy.loginfo('Attempting to grab ring...')
+        return 'success'
+
+class WaitForRelease(smach.State):
+    def __init__(self):
+        smach.State.__init__(self, outcomes=['success'])
+
+    def execute(self, userdata):
+        rospy.loginfo('Waiting for ring to be released...')
+        return 'success'
+
+class RingToPeg(smach.State):
+    def __init__(self):
+        smach.State.__init__(self, outcomes=['success'])
+
+    def execute(self, userdata):
+        rospy.loginfo('Moving ring to peg...')
+        return 'success'
+
+class DropRing(smach.State):
+    def __init__(self):
+        smach.State.__init__(self, outcomes=['success'])
+
+    def execute(self, userdata):
+        rospy.loginfo('Dropping ring...')
+        return 'success'
+
+if __name__ == '__main__':
+    rospy.init_node('smach_peg_task')
+
+    sm = smach.StateMachine(outcomes=['RING_TRANSFER_COMPLETE'])
+
+    with sm:
+            smach.StateMachine.add('WaitForRing', WaitForRing(),
+                    transitions={'success':'GrabRing'})
+            smach.StateMachine.add('GrabRing', GrabRing(),
+                    transitions={'success':'WaitForRelease'})
+            smach.StateMachine.add('WaitForRelease', WaitForRelease(),
+                    transitions={'success':'RingToPeg'})
+            smach.StateMachine.add('RingToPeg', RingToPeg(),
+                    transitions={'success':'DropRing'})
+            smach.StateMachine.add('DropRing', DropRing(),
+                    transitions={'success':'RING_TRANSFER_COMPLETE'})
+
+    outcome = sm.execute()
+
+    print outcome
+
