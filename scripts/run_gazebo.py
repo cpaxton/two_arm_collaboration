f9400360c86eec224120d8cec1c036891e5090ff
Christopher Paxton <cpaxton3@jhu.edu>
2014-05-26 16:26:47 -0400

    files for services, starting gazebo from frozen

--- scripts/run_gazebo.py
+++ scripts/run_gazebo.py
@@ -4,18 +4,23 @@ import roslib
 import sys
 import rospy
 
-from std_srvs import Empty
+from std_srvs.srv import Empty
 
 if __name__ == "__main__":
 
     rospy.init_node('run_gazebo_script_node')
 
-    # sleep for a bit
-    rospy.sleep(4.)
-
-    sname ="gazebo/unpause_physics"
+    print "Started ROS node waiting for Gazebo!"
+    sname ="/gazebo/unpause_physics"
     print "Waiting for service: %s"%sname
     rospy.wait_for_service(sname)
 
-    runscript = rospy.ServiceProxy(sname, Empty())
+    print "Sleeping..."
+
+    # sleep for a bit
+    #rospy.sleep(0.25)
+
+    print "Starting physics"
+    runscript = rospy.ServiceProxy(sname, Empty)
+    runscript()
 
