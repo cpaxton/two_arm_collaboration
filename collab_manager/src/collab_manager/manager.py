880653ae659a8c1eccb7ffb36b3799936bb87343
Christopher Paxton <cpaxton3@jhu.edu>
2014-07-03 11:28:55 -0400

    collab_manager to publish ik/gripper positions to the barrett arms

---
+++ collab_manager/src/collab_manager/manager.py
@@ -0,0 +1,16 @@
+
+from oro_barrett_msgs.msg import *
+from collab_msgs.srv import *
+from collab_msgs.msg import *
+import tf
+
+'''
+Publish the current gripper/IK commands to the arms
+'''
+class CollabManager(object):
+
+    __init__(self):
+        pass
+
+if __name__ == "__main__":
+    pass
