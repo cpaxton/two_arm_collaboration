4dd16e775524e4e7ac4e17366f94008f88fe130b
Christopher Paxton <cpaxton3@jhu.edu>
2014-07-22 09:47:28 -0400

    setup files for the package

--- collab_smach/src/collab_smach/__init__.py
+++ collab_smach/src/collab_smach/__init__.py
@@ -7,6 +7,8 @@ roslib.load_manifest('collab_smach')
 ### Available Classes for 'from beetree import *'
 __all__ = ['MoveToFrameNode']
 __all__ += ['MoveToObjectFrameNode']
+__all__ += ['ResetPoseNode']
 
 from move_to_frame import MoveToFrameNode
 from move_to_frame import MoveToObjectFrameNode
+from reset_pose import ResetPoseNode
