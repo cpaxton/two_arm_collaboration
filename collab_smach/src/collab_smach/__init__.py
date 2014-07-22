6e85f0f64061b44621456b617a68450a46008ba7
Christopher Paxton <cpaxton3@jhu.edu>
2014-07-22 09:29:36 -0400

    adding python files for SMACH execution

---
+++ collab_smach/src/collab_smach/__init__.py
@@ -0,0 +1,18 @@
+### ROS imports
+import roslib
+import rospy
+ 
+roslib.load_manifest('collab_smach')
+
+### Available Classes for 'from beetree import *'
+#__all__ = ['NodeConditionExistsGUI']
+#__all__ += ['NodeSetDestinationGUI']
+#__all__ += ['NodeMoveToDestinationGUI']
+#__all__ += ['NodeOpenGripperGUI']
+#__all__ += ['NodeCloseGripperGUI']
+
+#from condition_nodes import NodeConditionExistsGUI
+#from action_nodes import NodeSetDestinationGUI
+#from action_nodes import NodeMoveToDestinationGUI
+#from action_nodes import NodeOpenGripperGUI
+#from action_nodes import NodeCloseGripperGUI
