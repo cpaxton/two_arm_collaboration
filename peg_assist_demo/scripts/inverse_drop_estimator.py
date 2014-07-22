a15209ff75ab5e896ca17ea2aaf2f77b36590395
Christopher Paxton <cpaxton3@jhu.edu>
2014-07-22 14:14:56 -0400

    added more to the pose collector file

--- peg_assist_demo/scripts/inverse_drop_estimator.py
+++ peg_assist_demo/scripts/inverse_drop_estimator.py
@@ -3,9 +3,28 @@
 import rospy
 import tf
 
-
 if __name__ == "__main__":
+
+    rospy.init_node("inverse_drop_collector")
+    filename = rospy.get_param("~filename")
+    capture_rate = rospy.get_param("~rate",10)
+    frames_to_capture = rospy.get_param("~frames_to_capture",50)
+    wait_time = rospy.get_param("~wait", 5)
+
     rospy.loginfo("Starting inverse physics estimator for dropping objects")
 
+    rate = rospy.Rate(capture_rate)
 
+    rospy.sleep(wait_time)
+
+    # spin and write out a file
+    for i in range(0,frames_to_capture):
+
+        if rospy.is_shutdown():
+            break
+
+        rospy.loginfo("Capturing pose at step %d", i)
+        rate.sleep()
+
+    
 
