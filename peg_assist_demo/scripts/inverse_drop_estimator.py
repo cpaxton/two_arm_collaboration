4cb3b52001dba469f4bf0362b7e2545eb9cc399c
Christopher Paxton <cpaxton3@jhu.edu>
2014-07-28 16:04:27 -0400

    inverse drop thing works, but for some reason pegs fly away too

--- peg_assist_demo/scripts/inverse_drop_estimator.py
+++ peg_assist_demo/scripts/inverse_drop_estimator.py
@@ -3,30 +3,72 @@
 import rospy
 import tf
 
+from gazebo_msgs.srv import SetPhysicsProperties
+from geometry_msgs.msg import Vector3
+from gazebo_msgs.msg import ODEPhysics
+
 if __name__ == "__main__":
 
     rospy.init_node("inverse_drop_collector")
     filename = rospy.get_param("~filename")
     capture_rate = rospy.get_param("~rate",10)
-    frames_to_capture = rospy.get_param("~frames_to_capture",50)
-    wait_time = rospy.get_param("~wait", 5)
+    frames_to_capture = rospy.get_param("~frames_to_capture",10)
+    wait_time = rospy.get_param("~wait", 1.0)
+    frame = rospy.get_param("~frame", "ring1/ring_link")
 
-    rospy.loginfo("Starting inverse physics estimator for dropping objects")
+    try:
 
-    rate = rospy.Rate(capture_rate)
+        rospy.loginfo("Starting inverse physics estimator for dropping objects")
+        rate = rospy.Rate(capture_rate)
+        tfl = tf.TransformListener()
 
-    rospy.sleep(wait_time)
+        rospy.loginfo("Waiting for service to be available...")
+        rospy.wait_for_service('/gazebo/set_physics_properties')
 
-    tfl = tf.TransformListener()
+        rospy.loginfo("Additional sleep...")
+        rospy.sleep(wait_time)
 
-    # spin and write out a file
-    for i in range(0,frames_to_capture):
+        rospy.loginfo("Setting physics properties...")
+        srv = rospy.ServiceProxy("/gazebo/set_physics_properties", SetPhysicsProperties)
 
-        if rospy.is_shutdown():
-            break
+        time_step = 0.001
+        max_update_rate = 1000.0
+        gravity = Vector3()
+        gravity.x = 0
+        gravity.y = 0
+        gravity.z = 9.8
+        ode_config = ODEPhysics()
+        ode_config.auto_disable_bodies = False
+        ode_config.sor_pgs_precon_iters = 0
+        ode_config.sor_pgs_w = 1.3
+        ode_config.sor_pgs_rms_error_tol = 0.0
+        ode_config.contact_surface_layer = 0.001
+        ode_config.contact_max_correcting_vel = 100.0
+        ode_config.cfm = 0.0
+        ode_config.erp = 0.2
+        ode_config.max_contacts = 20
 
-        rospy.loginfo("Capturing pose at step %d", i)
-        rate.sleep()
+        srv(time_step, max_update_rate, gravity, ode_config)
 
-    
+        # spin and write out a file
+        for i in range(0,frames_to_capture):
 
+            if rospy.is_shutdown():
+                break
+
+            print "Target frame: " + frame + ", getting transform..."
+            tf_done = False
+
+            while not tf_done:
+                try:
+                    (trans, rot) = tfl.lookupTransform("/world", frame, rospy.Time(0))
+                    tf_done = True
+                except (tf.LookupException, tf.ConnectivityException, tf.ExtrapolationException):
+                    continue
+
+            print (trans, rot)
+
+            rospy.loginfo("Capturing pose at step %d", i)
+            rate.sleep()
+
+    except rospy.ROSInterruptException: pass
