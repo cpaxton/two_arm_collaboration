5eb5f6e55c1add1f2bec54bc5fc021950fcba49a
Christopher Paxton <cpaxton3@jhu.edu>
2014-09-09 13:48:24 -0400

    configured, get list of features to check, use get_key from core

--- peg_assist_demo/scripts/get_dense_binary_features.py
+++ peg_assist_demo/scripts/get_dense_binary_features.py
@@ -4,11 +4,14 @@ import rospy
 import rosbag
 
 from collab_msgs.msg import SegmentLabel
-from predictor_msgs.msg import PredicateList
+from predicator_msgs.msg import PredicateList
 
+import predicator_core
 
 X = [] # predicate features
 Y = [] # segment labels
+
+values = {}
 
 '''
 update_last_segment()
@@ -22,7 +25,9 @@ Go through the list and check for predicates we are interested in
 If they exist, add a 1; else add a 0
 '''
 def list_cb(msg):
-    pass
+    for item in msg.statements:
+        values[predicator_core.get_key(item.predicate, item.params)] = 0
+
 
 if __name__ == '__main__':
 
@@ -33,3 +38,11 @@ if __name__ == '__main__':
     segment_sub = rospy.Subscriber(segment_topic, SegmentLabel, update_last_segment)
     list_sub = rospy.Subscriber(predicate_topic, PredicateList, list_cb)
     
+    geo_frames = rospy.get_param("~geometry_frames")
+    geo_ref_frames = rospy.get_param("~geometry_reference_frames")
+    mv_frames = rospy.get_param("~movement_frames")
+    predicates = rospy.get_param("~predicates")
+
+    print predicates['geometry3']
+
+    rospy.spin()
