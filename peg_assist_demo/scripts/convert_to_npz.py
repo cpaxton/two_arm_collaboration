d91c44a4a25ec25687bd99962afd8a741d9efc49
Christopher Paxton <cpaxton3@jhu.edu>
2014-09-11 15:47:43 -0400

    conversion script updates

--- peg_assist_demo/scripts/convert_to_npz.py
+++ peg_assist_demo/scripts/convert_to_npz.py
@@ -3,19 +3,45 @@
 import rosbag
 import numpy as np
 import recognizer
+import sys
 
 if __name__ == '__main__':
     # get command line argument for file
 
-    if len(sys.argv) < 2:
+    if len(sys.argv) < 3:
         print "ERROR: not enough arguments!"
-        print "Required: [input_file] [output_file]"
+        print "REQUIRED: python convert_to_npz.py [input_file] [output_file]"
     else:
-        print sys.argv
 
         X = []
-        Y = []
-        labels = {}
+        Y_labels = []
+        max_inst = {}
         next_label = 0
 
-        rosbag.Bag(sys.argv[0], 'r')
+        bag = rosbag.Bag(sys.argv[1], 'r')
+
+        for topic, msg, t in bag:
+            if topic == '/binary_features':
+                name = msg.segment.segment_name
+                inst = msg.segment.segment_id
+                max_inst[name] = inst
+            else:
+                print "UNKNOWN TOPIC: " + topic
+
+        for topic, msg, t in bag:
+            if topic == '/binary_features':
+                name = msg.segment.segment_name
+                inst = msg.segment.segment_id
+                if inst == max_inst[name]:
+                    name = 'last_' + name
+                
+                arr = np.array(msg.value)
+                X.append(arr)
+
+                Y_labels.append(name)
+
+        data = {}
+        data['X'] = X
+        data['labels'] = Y_labels
+        filename = sys.argv[2]
+        np.savez(filename, data)
