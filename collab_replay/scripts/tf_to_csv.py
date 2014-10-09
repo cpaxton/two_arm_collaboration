9f7fd448b26515be84a0dec3ef83992191f5c80d
Christopher Paxton <cpaxton3@jhu.edu>
2014-10-09 16:37:57 -0400

    added tf script for experiments

---
+++ collab_replay/scripts/tf_to_csv.py
@@ -0,0 +1,43 @@
+#!/usr/bin/env python
+
+import tf
+import rospy
+import sys
+
+if __name__ == "__main__":
+    rospy.init_node("collab_tf_to_csv")
+
+    if len(sys.argv) < 2:
+        sys.exit("ERROR: missing output file name.")
+    elif len(sys.argv) < 3:
+        sys.exit("ERROR: missing first frame.")
+    elif len(sys.argv) < 4:
+        sys.exit("ERROR: missing second frame.")
+
+    filename = sys.argv[1]
+    frame1 = sys.argv[2]
+    frame2 = sys.argv[3]
+
+    rate = rospy.Rate(10.)
+
+    try:
+        f = open(filename, "w")
+        l = tf.TransformListener()
+
+        while not rospy.is_shutdown():
+            try:
+                (trans, rot) = l.lookupTransform(frame1,frame2,rospy.Time.now())
+                (r, p, y) = tf.transformations.euler_from_quaternion(rot)
+                res = ""
+                for t in trans:
+                    res = res + str(t) + ","
+                res = res + str(r) + "," + str(p) + "," + str(y);
+                f.write(res)
+            except (tf.LookupException, tf.ConnectivityException, tf.ExtrapolationException):
+                continue
+
+            rate.sleep()
+
+
+    except rospy.ROSInterruptException, e:
+        pass
